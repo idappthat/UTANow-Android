39f1de8cb92248cc35cc4af0126a45262544d9df
Rige02 <robert.aron.tx@verizon.net>
2016-02-02 15:00:39 -0600

    MapBox Added
    Mapbox is now the default activity. Loads into map showing arlington.
    Problem with pin not displaying text.
    
    Compiles and runs on my phone.

--- app/src/main/java/com/mobi/utanow/App/MapBox/MapBoxActivity.java
+++ app/src/main/java/com/mobi/utanow/App/MapBox/MapBoxActivity.java
@@ -1,31 +1,63 @@
 package com.mobi.utanow.App.MapBox;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
 import com.mobi.utanow.myapplication.R;
 
-public class MapBoxActivity extends AppCompatActivity {
-
+public class mapBoxActivity extends AppCompatActivity {
+    private MapView mapView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_box);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        /** Create a mapView and give it some properties */
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setStyleUrl(Style.DARK);
+        mapView.setCenterCoordinate(new LatLng(32.729,-97.115));
+        mapView.setZoomLevel(11);
+        mapView.addMarker(new MarkerOptions().title("Hello World!")
+                        .snippet("Party Here!!")
+                        .position(new LatLng(32.729,-97.115)));
+        mapView.onCreate(savedInstanceState);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
 }
