851a1a4d447bb4cdea14c2e5811a8f4a64d98df8
Rige02 <robert.aron.tx@verizon.net>
2016-02-05 16:12:19 -0600

    Login activity sorta set up
    Set up a function to open the map up by passing coordinates to it.

--- app/src/main/java/com/mobi/utanow/App/Login/LoginActivity.java
+++ app/src/main/java/com/mobi/utanow/App/Login/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.mobi.utanow.App.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.mobi.utanow.App.MapBox.MapBoxActivity;
+import com.mobi.utanow.myapplication.R;
 
 /**
  * Created by Anthony on 11/13/15.
@@ -10,6 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        setContentView(R.layout.activity_login);
 
+    }
+    public void goToMap(View view){//sorry about testing this way :<
+        Intent intent = new Intent(this, MapBoxActivity.class);
+        intent.putExtra("xco",(double)32.729);
+        intent.putExtra("yco",(double)-97.115);
+        startActivity(intent);
+    }
 }
