5826e7dbf84a2f7443f435970ab2d427336ad42f
Rige02 <robert.aron.tx@verizon.net>
2016-02-18 22:06:15 -0600

    I finished everything. About to check if it compiles

--- app/src/main/java/com/mobi/utanow/Adapter/favoritesAdapter.java
+++ app/src/main/java/com/mobi/utanow/Adapter/favoritesAdapter.java
@@ -1,7 +1,57 @@
 package com.mobi.utanow.Adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mobi.utanow.R;
+import com.mobi.utanow.models.EventModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by HeyImRige on 2/18/2016.
+ * Created by Robert on 2/18/2016.
  */
-public class FavoritesAdapter {
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.FavoriteHolder> {
+    List<EventModel> events;
+
+    public FavoritesAdapter(){
+        events = new ArrayList<>();
+    }
+
+    public void addFavorite(EventModel event){
+        events.add(event);
+        notifyItemInserted(events.size()-1);
+    }
+    @Override
+    public FavoriteHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_favorite,parent,false);
+        return new FavoriteHolder(itemView);
+    }
+    @Override
+    public void onBindViewHolder(FavoriteHolder holder, int position){
+        EventModel event = events.get(position);
+        holder.bind(event);
+    }
+    @Override
+    public int getItemCount(){return events.size();}
+
+    public class FavoriteHolder extends RecyclerView.ViewHolder{
+        TextView tvEvent, tvClub, tvDescription;
+
+        public FavoriteHolder(View itemView){
+            super(itemView);
+            tvEvent =(TextView) itemView.findViewById(R.id.event_Name);
+            tvClub =(TextView) itemView.findViewById(R.id.club_Name);
+            tvDescription =(TextView) itemView.findViewById(R.id.event_Description);
+        }
+        public void bind(EventModel event){
+            tvEvent.setText(event.getEventName());
+            tvClub.setText(event.getClubName());
+            tvDescription.setText(event.getDiscription());
+        }
+    }
 }
--- app/src/main/java/com/mobi/utanow/favorites/favoritesActivity.java
+++ app/src/main/java/com/mobi/utanow/favorites/favoritesActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.mobi.utanow.Adapter.FavoritesAdapter;
 import com.mobi.utanow.R;
+import com.mobi.utanow.models.EventModel;
 
 public class FavoritesActivity extends AppCompatActivity {
     FavoritesAdapter adapter;
@@ -20,6 +21,19 @@ public class FavoritesActivity extends AppCompatActivity {
         LinearLayoutManager lm = new LinearLayoutManager(this);
         adapter = new FavoritesAdapter();
 
-        //STOPED HERE
+        list = (RecyclerView) findViewById(R.id.list);
+        list.setHasFixedSize(true);
+        list.setLayoutManager(lm);
+        list.setAdapter(adapter);
+        addFakeEvents();
+    }
+
+
+    //ToDo actually recieve real information
+    public void addFakeEvents(){
+        adapter.addFavorite(new EventModel("BaseBall Game","Team Horses with Bats", "1....2.....3 strikes you're out!"));
+        adapter.addFavorite(new EventModel("Elite Four","Watch as Red fights rival", "Dude has legendary pokemon. Wont even be fair"));
+        adapter.addFavorite(new EventModel("UTA Big Band Concert","UTA MUSI Department", "Why is it a big band if it's smaller than a symphony?"));
+        adapter.addFavorite(new EventModel("Dope Party","Dope Party Club", "Actually it's a school sponsored so it's def not dope."));
     }
 }
