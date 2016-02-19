0bf414d683ad4398a8c6176b5603c8aeb8295652
Rige02 <robert.aron.tx@verizon.net>
2016-02-18 21:15:36 -0600

    started adapter have model and xml is being made

---
+++ app/src/main/java/com/mobi/utanow/Adapter/favoritesAdapter.java
@@ -0,0 +1,7 @@
+package com.mobi.utanow.Adapter;
+
+/**
+ * Created by HeyImRige on 2/18/2016.
+ */
+public class favoritesAdapter {
+}
---
+++ app/src/main/java/com/mobi/utanow/favorites/favoritesActivity.java
@@ -0,0 +1,13 @@
+package com.mobi.utanow.favorites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class favoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
