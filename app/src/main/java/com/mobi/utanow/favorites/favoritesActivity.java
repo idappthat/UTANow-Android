920e4af489af1adf8b341c82f58e434f4aea9ccf
Rige02 <robert.aron.tx@verizon.net>
2016-02-26 17:52:18 -0600

    hmmm
    whattt

--- app/src/main/java/com/mobi/utanow/favorites/favoritesActivity.java
+++ app/src/main/java/com/mobi/utanow/favorites/favoritesActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.mobi.utanow.Adapter.FavoritesAdapter;
 import com.mobi.utanow.R;
-import com.mobi.utanow.models.EventModel;
+import com.mobi.utanow.models.Event;
 
 public class FavoritesActivity extends AppCompatActivity {
     FavoritesAdapter adapter;
@@ -30,10 +29,11 @@ public class FavoritesActivity extends AppCompatActivity {
 
 
     //ToDo actually recieve real information
-    public void addFakeEvents(){
-        adapter.addFavorite(new EventModel("BaseBall Game","Team Horses with Bats", "1....2.....3 strikes you're out!"));
-        adapter.addFavorite(new EventModel("Elite Four","Watch as Red fights rival", "Dude has legendary pokemon. Wont even be fair"));
-        adapter.addFavorite(new EventModel("UTA Big Band Concert","UTA MUSI Department", "Why is it a big band if it's smaller than a symphony?"));
-        adapter.addFavorite(new EventModel("Dope Party","Dope Party Club", "Actually it's a school sponsored so it's def not dope."));
+    public void addFakeEvents()
+    {
+//        adapter.addFavorite(new Event("BaseBall Game","Team Horses with Bats", "1....2.....3 strikes you're out!"));
+//        adapter.addFavorite(new Event("Elite Four","Watch as Red fights rival", "Dude has legendary pokemon. Wont even be fair"));
+//        adapter.addFavorite(new Event("UTA Big Band Concert","UTA MUSI Department", "Why is it a big band if it's smaller than a symphony?"));
+//        adapter.addFavorite(new Event("Dope Party","Dope Party Club", "Actually it's a school sponsored so it's def not dope."));
     }
 }
